c78b946809f1f6469292a7894e39e38f04d17ccd
FelixGauer <felixgauer@gmx.net>
2023-12-28 15:43:45 +0100

    1st maze
    Changed Enemybehavior back to the old script, the new script with the random movement is just disabled.
    I did that so I could roughly test if the distance to the lever was long enough.
    
    I would be waiting for the new behavior now.

---
+++ Assets/Scripts/OldEnemyMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldEnemyMovement : MonoBehaviour
+{
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    // Use this for initialization
+    private void Start()
+    {
+
+        // Set position of Enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        // Move Enemy
+        Move();
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // Check if the enemy hasn't reached the last waypoint
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            // Move the enemy towards the current waypoint
+            transform.position = Vector2.MoveTowards(transform.position,
+                waypoints[waypointIndex].transform.position,
+                moveSpeed * Time.deltaTime);
+
+            // If the enemy reaches the current waypoint, move to the next one
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+        else
+        {
+            // Reset waypointIndex to 0 to loop back to the first waypoint
+            waypointIndex = 0;
+        }
+    }
+
+}
